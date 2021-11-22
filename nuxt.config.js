166d140fb4adb4643b7885cc538ccf7ddfbfe406
Pavel <68848355+Palchak-Pavel@users.noreply.github.com>
2021-11-22 10:18:21 +0300

    Авторизация и мелки исправления

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,136 +2,145 @@ import config from './src/configs'
 
 const { locale, availableLocales, fallbackLocale } = config.locales
 const { gaId } = config.analytics
+const isDev = process.env.NODE_ENV !== 'production'
 
 export default {
-  // ssr: false,
-  // target: 'static',
-  srcDir: 'src/',
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: {
-    titleTemplate: '%s - nuxt',
-    title: 'nuxt',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap' },
-      ...config.icons.map((href) => ({ rel: 'stylesheet', href }))
-    ]
-  },
+   // ssr: false,
+   // target: 'static',
+   srcDir: 'src/',
+   // Global page headers (https://go.nuxtjs.dev/config-head)
+   head: {
+      titleTemplate: '%s - nuxt',
+      title: 'nuxt',
+      meta: [
+         { charset: 'utf-8' },
+         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+         { hid: 'description', name: 'description', content: '' }
+      ],
+      link: [
+         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+         {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap'
+         },
+         ...config.icons.map((href) => ({ rel: 'stylesheet', href }))
+      ]
+   },
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-    '~/assets/scss/theme.scss',
-    '~/assets/scss/btn-style.scss',
-    '../node_modules/ag-grid-community/dist/styles/ag-grid.css',
-    '../node_modules/ag-grid-community/dist/styles/ag-theme-balham.css',
-    '../node_modules/ag-grid-community/dist/styles/ag-theme-alpine.css',
-    '../node_modules/ag-grid-community/dist/styles/ag-theme-blue.css',
-    '../node_modules/ag-grid-community/dist/styles/ag-theme-fresh.css',
-    '../node_modules/ag-grid-community/dist/styles/ag-theme-material.css',
+   // Global CSS (https://go.nuxtjs.dev/config-css)
+   css: [
+      '~/assets/scss/theme.scss',
+      '~/assets/scss/btn-style.scss',
+      '../node_modules/ag-grid-community/dist/styles/ag-grid.css',
+      '../node_modules/ag-grid-community/dist/styles/ag-theme-balham.css',
+      '../node_modules/ag-grid-community/dist/styles/ag-theme-alpine.css',
+      '../node_modules/ag-grid-community/dist/styles/ag-theme-blue.css',
+      '../node_modules/ag-grid-community/dist/styles/ag-theme-fresh.css',
+      '../node_modules/ag-grid-community/dist/styles/ag-theme-material.css'
 
-    // '@/node_modules/ag-grid-community/dist/styles/ag-grid.css',
-    // '@/node_modules/ag-grid-community/dist/styles/ag-theme-alpine.css'
+      // '@/node_modules/ag-grid-community/dist/styles/ag-grid.css',
+      // '@/node_modules/ag-grid-community/dist/styles/ag-theme-alpine.css'
 
-  ],
+   ],
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-    // plugins
-    { src: '~/plugins/animate.js', mode: 'client' },
-    { src: '~/plugins/apexcharts.js', mode: 'client' },
-    { src: '~/plugins/clipboard.js', mode: 'client' },
-    { src: '~/plugins/vue-shortkey.js', mode: 'client' },
-    {src: '~/plugins/ag_grid/ag-grid.orders.js', mode: 'client'},
-    {src: '~/plugins/order.js'},
-    {src: '~/plugins/shipments.js'},
-    {src: '~/plugins/leftovers.js'},
+   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+   plugins: [
+      // plugins
+      { src: '~/plugins/animate.js', mode: 'client' },
+      { src: '~/plugins/apexcharts.js', mode: 'client' },
+      { src: '~/plugins/clipboard.js', mode: 'client' },
+      { src: '~/plugins/vue-shortkey.js', mode: 'client' },
+      { src: '~/plugins/ag_grid/ag-grid.orders.js', mode: 'client' },
+      { src: '~/plugins/order.js' },
+      { src: '~/plugins/shipments.js' },
+      { src: '~/plugins/leftovers.js' },
 
-    // // // filters
-    { src: '~/filters/capitalize.js' },
-    { src: '~/filters/lowercase.js' },
-    { src: '~/filters/uppercase.js' },
-    { src: '~/filters/formatCurrency.js' },
-    { src: '~/filters/formatDate.js' },
-  ],
+      // // // filters
+      { src: '~/filters/capitalize.js' },
+      { src: '~/filters/lowercase.js' },
+      { src: '~/filters/uppercase.js' },
+      { src: '~/filters/formatCurrency.js' },
+      { src: '~/filters/formatDate.js' }
+   ],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  // components: true,
+   // Auto import components (https://go.nuxtjs.dev/config-components)
+   // components: true,
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/vuetify
-    ['@nuxtjs/vuetify', {
-      customVariables: ['~/assets/scss/vuetify/variables/_index.scss'],
-      optionsPath: '~/configs/vuetify.js',
-      treeShake: true,
-      defaultAssets: {
-        font: false
-      }
-    }],
-    ['nuxt-i18n', {
-      detectBrowserLanguage: {
-        useCookie: true,
-        cookieKey: 'i18n_redirected'
+   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+   buildModules: [
+      // https://go.nuxtjs.dev/vuetify
+      ['@nuxtjs/vuetify', {
+         customVariables: ['~/assets/scss/vuetify/variables/_index.scss'],
+         optionsPath: '~/configs/vuetify.js',
+         treeShake: true,
+         defaultAssets: {
+            font: false
+         }
+      }],
+      ['nuxt-i18n', {
+         detectBrowserLanguage: {
+            useCookie: true,
+            cookieKey: 'i18n_redirected'
+         },
+         locales: availableLocales,
+         lazy: true,
+         langDir: 'js/translations/',
+         defaultLocale: locale,
+         vueI18n: {
+            fallbackLocale
+         }
+      }]
+   ],
+
+   // Modules (https://go.nuxtjs.dev/config-modules)
+   modules: [
+      '@nuxtjs/google-gtag',
+      '@nuxtjs/axios',
+      '@nuxtjs/auth-next'
+   ],
+   axios: {
+      baseURL: 'http://192.168.0.155:8080'
+   },
+   'google-gtag': {
+      id: gaId,
+      debug: true, // enable to track in dev mode
+      disableAutoPageTrack: false // disable if you don't want to track each page route with router.afterEach(...).
+   },
+
+   // Build Configuration (https://go.nuxtjs.dev/config-build)
+   build: {
+      transpile: ['ag-grid-vue']
+   },
+
+   auth: {
+      strategies: {
+         local: {
+            token: {
+               property: 'access_token',
+               // required: true,
+               type: 'Bearer'
+            },
+            user: {
+               property: false, // <--- Default "user"
+               autoFetch: true
+            },
+            endpoints: {
+               login: { url: '/auth/account', method: 'post' },
+               logout: false,
+               user: { url: '/auth/account', method: 'get' }
+            }
+         }
       },
-      locales: availableLocales,
-      lazy: true,
-      langDir: 'js/translations/',
-      defaultLocale: locale,
-      vueI18n: {
-        fallbackLocale
+      redirect: {
+         login: '/auth/signin',
+         logout: '/',
+         callback: '/login',
+         home: '/'
+      },
+      router: {
+         prefetchLinks: false,
+         middleware: ['auth'],
+         base: !isDev ? '/crm' : ''
       }
-    }],
-  ],
-
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    '@nuxtjs/google-gtag',
-    '@nuxtjs/axios',
-    '@nuxtjs/auth-next',
-  ],
-  axios: {
-    baseURL: "http://192.168.0.155:8080"
-  },
-  'google-gtag': {
-    id: gaId,
-    debug: true, // enable to track in dev mode
-    disableAutoPageTrack: false // disable if you don't want to track each page route with router.afterEach(...).
-  },
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-    transpile: ['ag-grid-vue']
-  },
-
-  auth: {
-    strategies: {
-      local: {
-        token: {
-          property: 'access_token',
-          // required: true,
-          type: 'Bearer'
-        },
-        user: {
-          property: false, // <--- Default "user"
-          autoFetch: true
-        },
-        endpoints: {
-          login: { url: '/auth/account', method: 'post' },
-          logout: false,
-          user: { url: "/auth/account", method: 'get'}
-        }
-      }
-    },
-    redirect: {
-      login: '/auth/signin',
-      logout: '/',
-      callback: '/login',
-      home: '/'
-    }
-  },
+   }
 }
--- src/components/products/Orders.vue
+++ src/components/products/Orders.vue
@@ -1,77 +1,74 @@
 <template>
-  <v-col cols = "12">
-    <v-card flat>
-      <v-card-title>{{ $t('dashboard.orders') }}</v-card-title>
-      <v-card-text>
-        <client-only>
-          <v-btn class = "mr-1 mb-1 blue lighten-1 white--text" @click = "getOrders">{{
-              $t('common.refreshTable')
-                                                                                     }}
-          </v-btn>
+   <v-col cols = "12">
+      <v-card flat>
+         <v-card-title>{{ $t('dashboard.orders') }}</v-card-title>
+         <v-card-text>
+            <client-only>
+               <v-btn class = "mr-1 mb-1 blue lighten-1 white--text" @click = "getOrders">{{
+                     $t('common.refreshTable')
+                                                                                          }}
+               </v-btn>
 
-          <v-btn
-              class = "mr-1 mb-1 blue lighten-1 white--text"
-              dark
-              @click = "dialog = true"
-          >
-            {{ $t('common.uploadFile') }}
-          </v-btn>
+               <v-btn
+                 class = "mr-1 mb-1 blue lighten-1 white--text"
+                 dark
+                 @click = "dialog = true"
+               >
+                  {{ $t('common.uploadFile') }}
+               </v-btn>
 
-          <v-dialog
-              v-model = "dialog"
-              max-width = "500px"
-          >
-            <v-card>
-              <v-file-input
-                  v-model = "files"
-                  color = "blue lighten-1"
-                  class = "pl-2 pt-2 pr-2"
-                  label = "Выберите файл"
-                  prepend-icon = "mdi-paperclip"
-                  outlined
-                  :show-size = "1000"
-                  @change = "onFileChange"
-              >
-              </v-file-input>
+               <v-dialog
+                 v-model = "dialog"
+                 max-width = "500px"
+               >
+                  <v-card>
+                     <v-file-input
+                       v-model = "files"
+                       color = "blue lighten-1"
+                       class = "pl-2 pt-2 pr-2"
+                       label = "Выберите файл"
+                       prepend-icon = "mdi-paperclip"
+                       outlined
+                       :show-size = "1000"
+                       @change = "onFileChange"
+                     >
+                     </v-file-input>
 
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn
-                    class = "ml-1 mb-1 blue lighten-1 white--text"
-                    @click = "dialog = false"
-                >
-                  Закрыть
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
+                     <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                          class = "ml-1 mb-1 blue lighten-1 white--text"
+                          @click = "dialog = false"
+                        >
+                           Закрыть
+                        </v-btn>
+                     </v-card-actions>
+                  </v-card>
+               </v-dialog>
 
-          <v-btn @click = "'AG_GRID_LOCALE_RU'" color = "primary">
-            RU
-          </v-btn>
-          <v-btn @click = "'AG_GRID_LOCALE_EN'" color = "primary">
-            EN
-          </v-btn>
+               <v-btn @click = "localeTextFunc" color = "primary">
+                  RU
+               </v-btn>
 
-          <v-card flat>
-            <ag-grid-vue style = "width: 80vw; height: 70vh;"
-                         class = "ag-theme-balham"
-                         :columnDefs = "columnDefs"
-                         :rowData = "orders"
-                         :defaultColDef = "defaultColDef"
-                         :rowSelection = "rowSelectionType"
-                         :enableCellTextSelection = "true"
-                         :header-height = "50"
-                         :row-height = "40"
-                         :localeText = "localeText"
-                         :sideBar = "sideBar"
-            >
-            </ag-grid-vue>
-          </v-card>
-        </client-only>
-      </v-card-text>
-    </v-card>
-  </v-col>
+               <v-card flat>
+                  <ag-grid-vue style = "width: 80vw; height: 70vh;"
+                               class = "ag-theme-balham"
+                               :columnDefs = "columnDefs"
+                               :rowData = "orders"
+                               :defaultColDef = "defaultColDef"
+                               :rowSelection = "rowSelectionType"
+                               :enableCellTextSelection = "true"
+                               :header-height = "50"
+                               :row-height = "40"
+                               :localeTextFunc = "localeTextFunc"
+                               :sideBar = "sideBar"
+                  >
+                  </ag-grid-vue>
+               </v-card>
+            </client-only>
+         </v-card-text>
+      </v-card>
+   </v-col>
 </template>
 
 <script>
@@ -79,108 +76,111 @@ import 'ag-grid-enterprise'
 import { mapGetters } from 'vuex'
 import { funcTarget, parseTwoColumns } from '~/js/parser'
 import { AG_GRID_LOCALE_RU } from '../../js/translations/ruAgGrid'
+import {AG_GRID_LOCALE_EN} from '../../js/translations/default-locale-aggrid'
 
 export default {
-  data() {
-    return {
-      dialog: false,
-      files: [],
-      sideBar: null,
-      localeText: null,
-      rowData: null,
-      rowSelectionType: 'single',
-      moment: null,
-      cellRenderer: null,
-      columnDefs: [
-        {
-          headerName: `${this.$t('catalog.productCode')}`,
-          field: 'productCode',
-          filter: 'agSetColumnFilter'
+   data() {
+      return {
+         dialog: false,
+         files: [],
+         sideBar: null,
+         localeTextFunc: null,
+         localeText: null,
+         rowData: null,
+         rowSelectionType: 'single',
+         moment: null,
+         cellRenderer: null,
+         columnDefs: [
+            {
+               headerName: `${this.$t('catalog.productCode')}`,
+               field: 'productCode',
+               filter: 'agSetColumnFilter'
 
-        },
-        {
-          headerName: `${this.$t('ecommerce.orderQuantity')}`,
-          field: 'orderQuantity',
-          filter: 'agSetColumnFilter'
-        },
-        {
-          headerName: `${this.$t('ecommerce.completeQuantity')}`,
-          field: 'completeQuantity',
-          filter: true
-        },
-        {
-          headerName: `${this.$t('ecommerce.price')}`,
-          field: 'price',
-          filter: true
-        },
-        {
-          headerName: `${this.$t('catalog.creationDate')}`,
-          field: 'creationDate',
-          filter: 'agDateColumnFilter',
-          cellRenderer: (data) => {
-            return data.value ? (new Date(data.value)).toLocaleDateString() : ''
-          }
-        },
-        {
-          headerName: `${this.$t('catalog.readinessDate')}`,
-          field: 'readinessDate',
-          filter: true,
-          cellRenderer: (data) => {
-            return data.value ? (new Date(data.value)).toLocaleDateString() : ''
-          }
-        }
-      ],
+            },
+            {
+               headerName: `${this.$t('ecommerce.orderQuantity')}`,
+               field: 'orderQuantity',
+               filter: 'agSetColumnFilter'
+            },
+            {
+               headerName: `${this.$t('ecommerce.completeQuantity')}`,
+               field: 'completeQuantity',
+               filter: true
+            },
+            {
+               headerName: `${this.$t('ecommerce.price')}`,
+               field: 'price',
+               filter: true
+            },
+            {
+               headerName: `${this.$t('catalog.creationDate')}`,
+               field: 'creationDate',
+               filter: 'agDateColumnFilter',
+               cellRenderer: (data) => {
+                  return data.value ? (new Date(data.value)).toLocaleDateString() : ''
+               }
+            },
+            {
+               headerName: `${this.$t('catalog.readinessDate')}`,
+               field: 'readinessDate',
+               filter: true,
+               cellRenderer: (data) => {
+                  return data.value ? (new Date(data.value)).toLocaleDateString() : ''
+               }
+            }
+         ],
 
-      defaultColDef: {
-        minWidth: 130,
-        flex: 1,
-        sortable: true,
-        filterParams: { applyMiniFilterWhileTyping: true, buttons: ['clear', 'apply'] }
+         defaultColDef: {
+            minWidth: 130,
+            flex: 1,
+            sortable: true,
+            filterParams: { applyMiniFilterWhileTyping: true, buttons: ['clear', 'apply'] }
+         }
       }
-    }
-  },
+   },
 
-  beforeMount() {
-    this.localeText = AG_GRID_LOCALE_RU
+   beforeMount() {
+      // this.localeText = AG_GRID_LOCALE_RU
+      this.localeTextFunc= (key, defaultValue) => AG_GRID_LOCALE_RU[key] || AG_GRID_LOCALE_EN
 // this.sideBar =true;
 
-    // this.localeTextFunc = (key, defaultValue) => {
-    //   // const data = AG_GRID_LOCALE_RU;
-    //   // return data === key ? defaultValue : data;
-    //
-    //   // const gridKey = 'AG_GRID_LOCALE_RU' + key;
-    //   //
-    //   // // look the value up using an application wide service
-    //   // return applicationLocaleService(gridKey);
-    //   // return defaultValue ? AG_GRID_LOCALE_RU : '';
-    // };
-  },
-  computed: {
-    ...mapGetters('orders', ['orders'])
-  },
+      // this.localeTextFunc = (key, defaultValue) => {
+      //   // const data = AG_GRID_LOCALE_RU;
+      //   // return data === key ? defaultValue : data;
+      //
+      //   // const gridKey = 'AG_GRID_LOCALE_RU' + key;
+      //   //
+      //   // // look the value up using an application wide service
+      //   // return applicationLocaleService(gridKey);
+      //   // return defaultValue ? AG_GRID_LOCALE_RU : '';
+      // };
+   },
+   computed: {
+      ...mapGetters('orders', ['orders'])
+   },
 
-  methods: {
-    async getOrders() {
-      await this.$store.dispatch('orders/fetchOrders')
-    },
+   methods: {
+      async getOrders() {
+         await this.$store.dispatch('orders/fetchOrders')
+      },
 
-    async onFileChange(e) {
-      if (e) {
-        let parsedItems = await funcTarget(e, parseTwoColumns);
-        let items = parsedItems.map(x => ({
-          productCode: x.productCode,
-          storeQuantity: x.productCount
-        }));
-        let payload = {
-          supplierID: 3,
-          supplierRestChanges: items,
-          download: true
-        };
-        await this.$leftoversID.createLeftovers(payload);
-        await this.getLeftovers();
+      async onFileChange(e) {
+         if (e) {
+            let parsedItems = await funcTarget(e, parseTwoColumns)
+            let items = parsedItems.map(x => ({
+               productCode: x.productCode,
+               storeQuantity: x.productCount
+            }))
+            let payload = {
+               supplierID: 3,
+               supplierRestChanges: items,
+               download: true
+            }
+            await this.$leftoversID.createLeftovers(payload)
+            await this.getLeftovers()
+         }
       }
-    },
-  }
+   }
 }
 </script>
 
